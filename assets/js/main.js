948166c2967a7be04beffabe4b51cb751d168359
halil-akin <62850847+halil-akin@users.noreply.github.com>
2026-07-15 00:30:16 +0300

    v1.0.0 - Initial professional website

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+==========================================
+Halil AKIN Personal Website
+Version 1.0.0
+==========================================
+*/
+
+console.log("Halil AKIN Website v1.0.0");
